4592d00464d949677e16d261e1404fb045280afe
dev-tuhin01 <dev.tuhin.1@gmail.com>
2023-05-29 12:53:18 +0530

    completed file uploading system
    Users can upload image using client
    The image will be uploaded to the upload/ folder in server
    
    TODO: fix the display image system
    
    [Ticket:x]

--- Client/src/components/Imageloader.tsx
+++ Client/src/components/Imageloader.tsx
@@ -3,22 +3,35 @@ import "./Imageloader.css";
 import axios from "axios";
 
 export const Imageloader = () => {
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState<FormData | null>(null);
 
   const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     // @ts-ignore: Object is possibly 'null';
-    console.log(URL.createObjectURL(event.target!.files[0]));
+    // console.log(URL.createObjectURL(event.target!.files[0]));
     // @ts-ignore: Object is possibly 'null';
-    setImage(URL.createObjectURL(event.target!.files[0]));
+    // setImage(URL.createObjectURL(event.target!.files[0]));
+
+    const formData = new FormData();
+
+    // @ts-ignore: Object is possibly 'null';
+    formData.append(
+      "sample-image-file",
+      // @ts-ignore: Object is possibly 'null';
+      event.target.files[0],
+      // @ts-ignore: Object is possibly 'null';
+      event.target.files[0].name
+    );
+    console.log(formData.get("sample-image-file"));
+    setImage(formData);
   };
 
   const onSubmit = async (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     console.log("Submit button clicked!");
 
-    axios.post("http://localhost:4000/image-upload", {
-      //axios post request
-      value: "Hello World!",
+    axios.post("http://localhost:4000/image-upload", image).then((res) => {
+      console.log("Axios post request successful");
+      console.log(`Axios response ${res}`);
     });
   };
 
--- Server/index.ts
+++ Server/index.ts
@@ -3,11 +3,23 @@ import { Request } from "express";
 import { Response } from "express";
 import multer from "multer";
 import cors from "cors";
-const PORT = 4000 || process.env.IP;
-const app = express();
-const upload = multer({
-  dest: "./upload/",
+import bodyParser from "body-parser";
+
+const PORT = 4000; //Port where the server will be hosted
+const app = express(); //initializing express
+
+const imageUploadPath = "./upload"; //path where the image will be loaded
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, imageUploadPath);
+  },
+  filename: function(req, file, cb) {
+    cb(null, `${file.fieldname}_dateVal_${Date.now()}_${file.originalname}`);
+  },
 });
+
+const imageUpload = multer({ storage: storage }); //initializing multer
 
 //adding Cors
 const corsOrigin = "http://localhost:5173";
@@ -20,16 +32,24 @@ app.use(
   })
 );
 
+//adding body-parser code
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.set("Access-Control-Allow-Origin", "http://localhost:5173/");
   res.send("working");
 });
 
-app.post("/image-upload", (req: Request, res: Response) => {
-  console.log("App is running");
-  console.log(`Axios POST body: ${req.body}`);
-  res.send("POST request recieved on server to /image-upload.");
-});
+app.post(
+  "/image-upload",
+  imageUpload.array("sample-image-file"),
+  (req: Request, res: Response) => {
+    console.log("App is running");
+    console.log(`Axios POST body: ${req.body}`);
+    res.send("POST request recieved on server to /image-upload.");
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`app is running on port : http://localhost:${PORT}`);
