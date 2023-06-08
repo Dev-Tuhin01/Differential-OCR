30e7a2ed4a0747ece9c4fedf46fc3a5aa08e8b1c
dev-tuhin01 <dev.tuhin.1@gmail.com>
2023-06-09 00:05:02 +0530

    Implementing OCR
    preprocessed the Image using grapgicsMagick
    OCRed the preprocessed image using tesseract JS
    TODO:complete the segmentation
    
    [Ticket:x]

--- Server/index.ts
+++ Server/index.ts
@@ -1,23 +1,29 @@
 import express from "express";
-import { Request } from "express";
-import { Response } from "express";
+import { Request, Response } from "express";
 import multer from "multer";
 import cors from "cors";
 import bodyParser from "body-parser";
 import path from "path";
+import { createWorker } from "tesseract.js";
+import gm from "gm";
 
 const PORT = 4000; //Port where the server will be hosted
 const app = express(); //initializing express
 const date = new Date();
 const imageUploadPath = "./upload"; //path where the image will be loaded
 
-let imageName = "test" + date.getTime();
+let imageName = "";
+function getImageName(name: string) {
+  imageName = name;
+}
+
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, imageUploadPath);
   },
   filename: function(req, file, cb) {
-    imageName.concat(path.extname(file.originalname));
+    const imageName = "test" + date.getTime() + path.extname(file.originalname);
+    getImageName(imageName);
     cb(null, `${imageName}`);
   },
 });
@@ -48,12 +54,57 @@ app.get("/", (req: Request, res: Response) => {
 app.post(
   "/image-upload",
   imageUpload.array("sample-image-file"),
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     console.log("App is running");
-    console.log(`Axios POST body: ${req.body}`);
+    console.dir(`Axios POST body: ${req.body}`);
     res.send("POST request recieved on server to /image-upload.");
+
+    try {
+      //@ts-ignore:
+      const imagePath = req.file.path;
+
+      //image preprocessing
+      gm(imagePath)
+        .noise("gaussian")
+        .resize(1000, 1000)
+        .contrast(-5)
+        .write(imagePath, async (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: "failed to preprocess the image." });
+            return;
+          }
+
+          //performing OCR on the preprocessed image
+
+          const worker = await createWorker({
+            logger: (m) => console.log(m),
+          });
+
+          await worker.load();
+          await worker.loadLanguage("eng");
+          await worker.initialize("eng");
+          const { data } = await worker.recognize(imagePath);
+          const { text } = data;
+          await worker.terminate();
+
+          //perform segmentation and symbol reconiztion
+          const segmentedText = performSegmentationAndSymbolRecognition(text);
+
+          res.json({ text: segmentedText });
+        });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "failed to perform OCR on this page" });
+    }
   }
 );
+
+function performSegmentationAndSymbolRecognition(text: string) {
+  //Regular expressions to
+  //
+  return segmentendText;
+}
 
 app.listen(PORT, () => {
   console.log(`app is running on port : http://localhost:${PORT}`);
